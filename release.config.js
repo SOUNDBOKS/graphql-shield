77b99026f285f5fbd4ab1a6ee7d1db2481701538
Rasmus Letterkrantz <rasmus.letterkrantz@gmail.com>
2025-09-03 11:20:10 +0200

    fix: move to .js file for semmantic release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    '@semantic-release/git',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        pkgRoot: 'dist',
+      },
+    ],
+  ],
+};
